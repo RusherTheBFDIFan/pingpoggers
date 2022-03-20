9ef99b225ef859dfe15e896b0896e18bc3ab8c9e
RusherTheBFDIFan <78630521+RusherTheBFDIFan@users.noreply.github.com>
2022-03-19 22:27:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+cocossd = ""
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function start() {
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
